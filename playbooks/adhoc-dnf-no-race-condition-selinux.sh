c26c1bebb6d7aa6a815a6a6322eaa8ae341279d9
john <john@devuan-hp-elitebook-001>
2019-08-09 10:48:38 -0700

    Added comments to help with switch understanding"

--- playbooks/adhoc-dnf-no-race-condition-selinux.sh
+++ playbooks/adhoc-dnf-no-race-condition-selinux.sh
@@ -1,1 +1,7 @@
-ansible webapp web -m dnf -a "name=libselinux-python state=present" -f 1
+ansible webapp web -m dnf -a "name=libselinux-python state=present" -f 1 -b
+# -f is the max number of concurrent hosts
+# limits by size of play or task targets
+# further limited by serial batch size
+
+# -b is to use elevated privileges because user doesn't have permission 
+# to install packages
